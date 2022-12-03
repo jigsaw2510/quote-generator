506c61006b0a214fc8089bf4037a5540d24f3ff5
jigsaw2510 <cata_bb92@yahoo.com>
2022-12-03 09:04:19 +0200

    added loader functionality

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
-
+const loader = document.getElementById('loader');
 
 
 
@@ -11,8 +11,21 @@ const newQuoteBtn = document.getElementById('new-quote');
 
 let apiQuotes = [];
 
+// Show Loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
 // Show New Quote
 function newQuote() {
+    loading();
     // Pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     // Check if author field is blank and replace it with 'Unknown'
@@ -27,12 +40,15 @@ function newQuote() {
     } else {
         quoteText.classList.remove('long-quote');
     }
+    // Set Quote, Hide Loader
     quoteText.textContent = quote.text;
+    complete();
 }
 
 // Get Quotes from API
 
 async function getQuotes() {
+    loading();
     // const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
     const apiUrl = 'https://type.fit/api/quotes';
     try {
